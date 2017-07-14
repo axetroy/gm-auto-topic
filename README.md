ac421cd92979b8fc6ade3ad27c08d8aaa038f9db
axetroy <troy450409405@gmail.com>
2017-07-14 18:41:18 +0800

    init

---
+++ README.md
@@ -0,0 +1,68 @@
+### gm-auto-topic
+[![Build Status](https://travis-ci.org/axetroy/gm-auto-topic.svg?branch=master)](https://travis-ci.org/axetroy/gm-auto-topic)
+[![npm version](https://badge.fury.io/js/gm-auto-topic.svg)](https://badge.fury.io/js/gm-auto-topic)
+[![Greenkeeper badge](https://badges.greenkeeper.io/axetroy/gm-auto-topic.svg)](https://greenkeeper.io/)
+![Node](https://img.shields.io/badge/node-%3E=6.0-blue.svg?style=flat-square)
+![License](https://img.shields.io/badge/license-MIT-green.svg)
+
+gm脚本的模板
+
+### 反馈地址
+
+- https://github.com/axetroy/gm-auto-topic/issues
+- https://github.com/axetroy/gm-auto-topic/issues
+- https://github.com/axetroy/gm-auto-topic/issues
+
+### 如果这能够帮助到你, 不妨点个start, 你的支持就是我更新的动力
+
+### 使用
+
+1. 安装扩展
+
+安装前确保你的浏览器已安装如下扩展
+
+浏览器 | 支持扩展
+------------ | -------------
+Chrome | **Tampermonkey** or **Violent monkey**
+Firefox | **Greasemonkey** or **Tampermonkey**
+Safari | **Tampermonkey**
+Microsoft Edge | **Tampermonkey**
+Opera | **Tampermonkey**
+Maxthon | **Violentmonkey**
+Dolphin | **Tampermonkey**
+UC | **Tampermonkey**
+Qupzilla | 不需要额外软件
+
+2. 安装脚本
+
+[点击从Github安装](https://github.com/axetroy/gm-auto-topic/raw/gh-pages/gm-auto-topic.min.user.js)
+
+[点击从GreasyFork安装](https://greasyfork.org/scripts/11915-gm-auto-topic-typescript/code/gm-auto-topic%20(typescript).user.js)
+
+### 支持的站点
+
+```javascript
+// @include           *
+```
+
+### 贡献代码
+
+```bash
+git clone https://github.com/axetroy/gm-auto-topic.git
+
+cd ./gm-auto-topic
+
+npm install
+npm run start
+```
+
+### 贡献者
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+| [<img src="https://avatars1.githubusercontent.com/u/9758711?v=3" width="100px;"/><br /><sub>Axetroy</sub>](http://axetroy.github.io)<br />[💻](https://github.com/axetroyts-gmscript-boilerplate/gm-auto-topic/commits?author=axetroy) [🐛](https://github.com/axetroyts-gmscript-boilerplate/gm-auto-topic/issues?q=author%3Aaxetroy) 🎨 |
+| :---: |
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+## 开源许可
+
+The [MIT License](https://github.com/axetroy/gm-auto-topic/blob/master/LICENSE)
---
+++ index.ts
@@ -0,0 +1,111 @@
+import http from './lib/http';
+import has = Reflect.has;
+
+class Topic {
+  constructor() {
+    this.makeSureFetchButton();
+    this.onStateChange(() => {
+      const timer = setInterval(() => {
+        clearInterval(timer);
+        this.makeSureFetchButton();
+      }, 500);
+    });
+  }
+  makeSureFetchButton() {
+    const $topicsContainer = document.getElementById('topics-list-container');
+    const $manageTopics = document.querySelector('.js-repo-topics-form-toggle');
+    if (!$topicsContainer || !$manageTopics) return;
+    $topicsContainer.appendChild(this.createButton());
+  }
+  createButton() {
+    const $button: HTMLButtonElement = document.createElement('button');
+    $button.className = 'btn-link';
+    $button.type = 'button';
+    $button.id = 'fetch-from-package';
+    $button.style.color = '#4CAF50';
+    $button.innerHTML = 'Fetch from package.json';
+    $button.onclick = this.onclickHandler.bind(this);
+    return $button;
+  }
+
+  private onStateChange(func) {
+    addEventListener(
+      'click',
+      event => {
+        const $done = document.querySelector('button.js-repo-topics-form-done');
+        if (event.target !== $done) return;
+        func(event);
+      },
+      false
+    );
+  }
+
+  async onclickHandler(event) {
+    event.preventDefault();
+    const $button = <HTMLButtonElement>document.querySelector(
+      '.btn-link.js-repo-topics-form-toggle.js-details-target'
+    );
+
+    if (!$button)
+      return console.warn(`Can't found the button to show topic...`);
+
+    $button.click();
+
+    let packageJson: Object = {};
+
+    try {
+      packageJson = await this.fetchPackage();
+    } catch (err) {
+      console.error(err);
+    }
+
+    function check() {
+      const $formUl = <HTMLUListElement>document.querySelector(
+        '.js-tag-input-selected-tags'
+      );
+      if (!$formUl) return;
+      clearInterval(timer);
+
+      const formValues: string[] = Array.from(
+        $formUl.querySelectorAll('input')
+      ).map($input => $input.value);
+
+      const keywords = packageJson['keywords'] || [];
+
+      const $input = <HTMLInputElement>document.querySelector('#repo_topics');
+
+      keywords
+        .filter(kw => kw)
+        .filter(kw => formValues.findIndex(v => v === kw) < 0)
+        .forEach(keyword => {
+          $input.value = keyword.replace(/\./g, '-');
+          $input.click();
+          $input.blur();
+        });
+      $input.focus();
+    }
+
+    const timer = setInterval(check, 500);
+  }
+
+  async fetchPackage(): Promise<any> {
+    const $package = <HTMLAnchorElement>document.querySelector(
+      'tr.js-navigation-item td.content a[title="package.json"]'
+    );
+    let packageUrl = $package.href;
+    packageUrl =
+      packageUrl
+        .replace('/blob/', '/')
+        .replace('github', 'raw.githubusercontent') + `?v=${Math.random()}`;
+    let res: any = {};
+    try {
+      res = await http.get(packageUrl);
+      res = JSON.parse(res.response);
+    } catch (err) {
+      console.error(err);
+    }
+    return res;
+  }
+}
+
+new Topic();
